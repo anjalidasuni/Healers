31f6b3a6763d9f09269b0ecb835f93095875b835
Anjali Dasuni <69907262+anjalidasuni@users.noreply.github.com>
2022-06-18 23:54:53 +0530

    Add files via upload

---
+++ FULL STACK WEB DEVELOPMENT/contact1.php
@@ -0,0 +1,39 @@
+<?php
+   if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message =$_POST['message'];
+    
+  
+  // Connecting to the Database
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "contact1";
+
+  // Create a connection
+  $conn = mysqli_connect($servername, $username, $password, $database);
+  // Die if connection was not successful
+  if(!$conn){
+      die("Sorry we failed to connect: ". mysqli_connect_error());
+  }
+  else{ 
+    // Submit these to a database
+    // Sql query to be executed 
+    $sql = "INSERT INTO `users` (`name`, `email`, `message`) VALUES ('$name', '$email','$message')";
+    $result = mysqli_query($conn, $sql);
+
+if($result){
+      echo "<script> alert(' Your data has been send...! ')</script>";
+    }
+
+        // echo "The record was not inserted successfully because of this error ---> ". mysqli_error($conn);
+        else{
+            // echo "The record was not inserted successfully because of this error ---> ". mysqli_error($conn);
+             echo "<script> alert(' Your data is not send. Please try again ')</script>";
+        }
+
+      }
+
+    } 
+  ?>
---
+++ FULL STACK WEB DEVELOPMENT/frontpage.php
@@ -0,0 +1,361 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title >Technical Era</title>
+    <link rel="stylesheet" href="frontpage.css">
+</head>
+<body>
+
+    <!-- Header -->
+    
+        <div class="header " id="header">
+            <div class="container header_container">
+                <div class="logo">
+                   <h1> Healers </h1>
+                </div>
+                <div class="nav">
+                    <div class="nav-item"><a href="#section1" style="text-decoration:none; color:white;">Home</a></div>
+                    <div class="nav-item"><a href="#section2" style="text-decoration:none; color:white;">About</a></div>
+                    <div class="nav-item"><a href="#section3" style="text-decoration:none; color:white;">services</a></div>
+                    <div class="nav-item"><a href="#section4" style="text-decoration:none; color:white;">Portfolio</a></div>
+                    <div class="nav-item"><a href="#section5" style="text-decoration:none; color:white;">Contact</a></div>
+                    <div class="nav-item" ><a href="logout.php" style="text-decoration:none; color:white;">Logout</a></div>
+                    
+                </div>
+
+            </div>
+
+        </div>
+          <!-- Header end -->
+
+          <!-- banner -->
+          <div class="banner" id="section1">
+              <div class="overlay">
+                  <div class="banner_text">
+                      <div class="padd">
+                          <h1 class="banner_title"> Build with Us</h1>
+                        <p class="banner_desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti accusamus cupiditate magnam, aliquid consequuntur, asperiores veritatis sint expedita aut ullam facilis! Voluptates unde harum eos quibusdam, qui a ex minus.
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, omnis modi aspernatur eum autem sunt placeat molestiae! Neque omnis quod, laborum perferendis adipisci magni atque. Odio corporis quae provident distinctio.
+
+                        </p>
+                        <p>
+                            <button class="banner_button">Start Working</button>
+                        </p>
+                        </div>
+                  </div>
+                </div>
+                
+            </div>
+
+          <!-- banner end -->
+
+          <!-- services -->
+
+          <div class="services" id="section3">
+        <h1> What We Do</h1>
+        <div class="container service_container">
+            <div class="service">
+                <div class="service_img">
+                    <img src="https://images.pexels.com/photos/7176323/pexels-photo-7176323.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="img">
+
+                </div> 
+               
+                    <div class="service_details"> 
+                        <div class="service_padd">
+                        <h1 class="service_title"> Counseling </h1>
+                        <p class="service_description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam maxime deleniti qui, natus consectetur reiciendis possimus iste rem voluptatibus dolor obcaecati eveniet corrupti dignissimos debitis laudantium commodi quae reprehenderit eos.
+                            met consectetur adipisicing elit. Magnam maxi
+                        </p>
+                        <p>
+                            <button class="service_button">
+                                Read More
+
+                            </button>
+                        </p>
+                    </div>
+                
+
+            </div>
+        </div>
+
+        <div class="service">
+            <div class="service_img">
+                <img src="https://images.pexels.com/photos/1709003/pexels-photo-1709003.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"  alt="img">
+
+            </div> 
+           
+                <div class="service_details"> 
+                    <div class="service_padd">
+                    <h1 class="service_title"> Conduct Seminars </h1>
+                    <p class="service_description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam maxime deleniti qui, natus consectetur reiciendis possimus iste rem voluptatibus dolor obcaecati eveniet corrupti dignissimos debitis laudantium commodi quae reprehenderit eos.
+                        met consectetur adipisicing elit. Magnam maxi
+                    </p>
+                    <p>
+                        <button class="service_button">
+                            Read More
+
+                        </button>
+                    </p>
+                </div>
+            
+
+        </div>
+    </div>
+    <div class="service">
+        <div class="service_img">
+            <img src="https://images.pexels.com/photos/9538589/pexels-photo-9538589.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="img">
+
+        </div> 
+       
+            <div class="service_details"> 
+                <div class="service_padd">
+                <h1 class="service_title"> Opportunities</h1>
+                <p class="service_description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam maxime deleniti qui, natus consectetur reiciendis possimus iste rem voluptatibus dolor obcaecati eveniet corrupti dignissimos debitis laudantium commodi quae reprehenderit eos.
+                    met consectetur adipisicing elit. Magnam maxi
+                </p>
+                <p>
+                    <button class="service_button">
+                        Read More
+
+                    </button>
+                </p>
+            </div>
+        
+
+    </div>
+</div>
+<div class="service">
+    <div class="service_img">
+        <img src="https://images.pexels.com/photos/45842/clasped-hands-comfort-hands-people-45842.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="img">
+
+    </div> 
+   
+        <div class="service_details"> 
+            <div class="service_padd">
+            <h1 class="service_title">Support</h1>
+            <p class="service_description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam maxime deleniti qui, natus consectetur reiciendis possimus iste rem voluptatibus dolor obcaecati eveniet corrupti dignissimos debitis laudantium commodi quae reprehenderit eos.
+                met consectetur adipisicing elit. Magnam maxi
+            </p>
+            <p>
+                <button class="service_button">
+                    Read More
+
+                </button>
+            </p>
+        </div>
+    
+
+</div>
+</div>
+          </div>
+        </div>
+
+       <!-- who we are -->
+       <div class="about" id="section2">
+           <h1>Who Are We ?</h1>
+           <div class="container about_container">
+           <span class="start_text">Healer</span> is an unlicensed person who practices the art of healing using traditional practices, herbal remedies and the power of suggestion. The healer may be a highly trained person who pursues their specialties, learning by study, observation and imitation.
+     Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act. It also helps determine how we handle stress, relate to others, and make healthy choices. Mental health is important at every stage of life, from childhood and adolescence through adulthood.
+     This misconception is one of the most harmful because it implies that the symptoms surrounding mental illnesses are not valid, which makes those who are living with one feel like they don't matter.
+     Emotional and mental health is important because it's a vital part of your life and impacts your thoughts, behaviors and emotions. Being healthy emotionally can promote productivity and effectiveness in activities like work, school or caregiving.
+     Mental health includes emotional, psychological, and social well-being. It affects how we think, feel, act, make choices, and relate to others. Mental health is more than the absence of a mental illness—it’s essential to your overall health and quality of life. Self-care can play a role in maintaining your mental health and help support your treatment and recovery if you have a mental illness.
+     Self-care means taking the time to do things that help you live well and improve both your physical health and mental health. When it comes to your mental health, self-care can help you manage stress, lower your risk of illness, and increase your energy. Even small acts of self-care in your daily life can have a big impact.
+     Seek professional help if you are experiencing severe or distressing symptoms that have lasted two weeks or more, such as:
+
+Difficulty sleeping
+Appetite changes that result in unwanted weight changes
+Struggling to get out of bed in the morning because of mood
+Difficulty concentrating
+Loss of interest in things you usually find enjoyable
+Inability to perform usual daily functions and responsibilities
+Look around (like, literally look around or scan your timeline or Twitter feed). You may have noticed that we're making huge strides in destigmatizing mental illness, and that's fantastic. The importance of treating your mental health as you would your physical health is a pretty well accepted principle. It's becoming less taboo to talk openly about therapy and mental illness—there are almost as many celebrities talking about mental health as there are Keep Calm and Whatever decorations in literally any store you walk into. All signs point to the fact that we're prioritizing self care, and that's awesome.
+
+But when it comes to actually being mentally well, we're a little murky on the follow-through. There's no shortage of self-care strategies out there, but not everyone has the time and money to spend on a wellness retreat or horse therapy or crystals that may or may not chill you out.
+       </div>
+        </div>
+
+        <!-- highlight -->
+
+        <div class="highlight ">
+            <div class="container highlight_container">
+            <div class="highlight_ele">
+                <div class="high_ele_child">
+                <center><h1 class="high_title"> 500+</h1></center>
+                <p class="high_desc"> Happy patients</p>
+            </div>
+        </div>
+            <div class="highlight_ele">
+                <div class="high_ele_child">
+                <center><h1 class="high_title" > 30+</h1></center>
+                <p class=high_desc> Doctors </p>
+            </div>
+            </div>
+            <div class="highlight_ele">
+                <div class="high_ele_child">
+                <center><h1 class="high_title" > 255+</h1></center>
+                <p class=high_desc>Reviews</p>
+            </div>
+            </div>
+            <div class="highlight_ele">
+                <div class="high_ele_child">
+                <center><h1 class="high_title" > 50+</h1></center>
+                <p class=high_desc>Members</p>
+            </div>
+            </div>
+
+        </div>
+  </div>
+
+  <!-- get a quote -->
+  <div class="form_layout" id="section5">
+  <form class="form" method="POST" action="/FULL STACK WEB DEVELOPMENT/contact.php" name="myform" onsubmit="popup()">
+    <center><h2 class="form_h2">CONTACT US</h2></center>
+    
+    <p class="form_para" type="Name:"><br><input class="form_input" placeholder="Write your name here.." name="name" ></input></p>
+    <p class="form_para" type="Email:"><br><input class="form_input" placeholder="Let us know how to contact you back.." name="email"></input></p>
+    <p  class="form_para" type="Message:"><br><input class="form_input"  placeholder="What would you like to tell us.." name="message"></input></p>
+    <button class="form_button">Send Message</button>
+    <div class="form_div">
+      <span class="fa fa-phone"></span>001 1023 567
+      <span class="fa fa-envelope-o"></span> contact@company.com
+    </div>
+  </form>
+  
+</div>
+
+
+<!-- code for portfolio -->
+            
+<div class="portfolio" id="section4">
+    <h1>Our Work</h1><br>
+    <div class="container portfolio_container">
+        <div class="project project1">
+            <div class="project_title">
+             <h1>
+                Oragnisations
+             </h1>
+            </div>
+        </div>
+        <div class="project project2">
+            <div class="project_title">
+                <h1>
+               Projects
+                </h1>
+               </div>
+        </div>
+        <div class="project project3">
+            <div class="project_title">
+                <h1>
+                Seminars
+                </h1>
+               </div>
+        </div>
+        <div class="project project4">
+            <div class="project_title">
+                <h1>
+               Programs 
+                </h1>
+               </div>
+        </div>
+        <div class="project project5">
+            <div class="project_title">
+                <h1>
+                    Camps
+                </h1>
+               </div>
+        </div>
+        <div class="project project6">
+            <div class="project_title">
+                <h1>
+                  Jobs  
+                </h1>
+               </div>
+        </div>
+        <div class="project project7">
+            <div class="project_title">
+                <h1>
+                    NGO
+                </h1>
+               </div>
+        </div>
+        <div class="project project8">
+            <div class="project_title">
+                <h1>
+               About benefitted patient
+                </h1>
+               </div>
+        </div>
+
+    </div>
+
+</div>
+
+<!-- Ask anything -->
+      
+
+
+ <!-- Footer -->
+
+ <div class="footer">
+     <div class="container footer_container">
+         <div class="footer_child footer_child1">
+             <h1 class ="footer_logo">Let us help you</h1>
+         </div>
+         <div class="footer_child">
+             <ul>
+                 <li class="first">Quick Links</li>
+                 <li>Home</li>
+                 <li>Services</li>
+                 <li>About</li>
+                 <li>Portfolio</li>
+                <li>Contacts</li>
+             </ul>
+
+         </div>
+         <div class="footer_child" id="contactss">
+             <ul>
+                 <li class="first">
+                     Contacts Us
+                 </li>
+                 <li>
+                     Haldwani Nainital
+                     Uttarakhand , India
+                 </li>
+                 <li> 1234567891</li>
+                 <li>info@123.com</li>
+             </ul>
+         </div>
+         <div class="footer_child footer_child_social ">
+             <div class="container container_social ">
+                 <div class="social_icon si1"></div>
+                 <div class="social_icon si2"></div>
+                 <div class="social_icon si3"></div>
+                 <div class="social_icon si4"></div>
+
+             </div>
+             
+         </div>
+     </div>
+ </div>
+ <script>
+     window.onscroll=function()
+     {
+         if(window.pageYOffset>10){
+             document.getElementById('header').style.backgroundColor="#363535"
+
+         }
+         else
+         {
+            document.getElementById('header').style.backgroundColor="transparent"
+
+         }
+     }
+ </script>
+
+            
+    </body>
+
+</html>
